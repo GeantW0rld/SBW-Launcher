dbfbf3f8554eb3f72f0cd1fc1a6a876bb59965b8
GeantW0rld <106020834+GeantW0rld@users.noreply.github.com>
2024-05-12 19:47:38 +0200

    update rss

--- src/assets/js/utils/config.js
+++ src/assets/js/utils/config.js
@@ -9,7 +9,7 @@ const convert = require('xml-js');
 let url = pkg.user ? `${pkg.url}/${pkg.user}` : pkg.url
 
 let config = `${url}/launcher/config-launcher/config.json`;
-let news = `${url}/launcher/news-launcher/news.json`;
+let news = `https://geantworld.c1.is/api/rss`;
 
 class Config {
     GetConfig() {
