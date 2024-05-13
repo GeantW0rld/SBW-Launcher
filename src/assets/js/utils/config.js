f664e240e20f16f00bdc8298a76e44758f605d72
GeantW0rld <106020834+GeantW0rld@users.noreply.github.com>
2024-05-13 20:08:39 +0200

    fixed RSS

--- src/assets/js/utils/config.js
+++ src/assets/js/utils/config.js
@@ -38,40 +38,44 @@ class Config {
     }
 
     async getNews() {
-        let config = await this.GetConfig() || {}
-
-        if (config.rss) {
-            return new Promise((resolve, reject) => {
-                nodeFetch(config.rss).then(async config => {
-                    if (config.status === 200) {
-                        let news = [];
-                        let response = await config.text()
-                        response = (JSON.parse(convert.xml2json(response, { compact: true })))?.rss?.channel?.item;
-
-                        if (!Array.isArray(response)) response = [response];
-                        for (let item of response) {
-                            news.push({
-                                title: item.title._text,
-                                content: item['content:encoded']._text,
-                                author: item['dc:creator']._text,
-                                publish_date: item.pubDate._text
-                            })
-                        }
-                        return resolve(news);
-                    }
-                    else return reject({ error: { code: config.statusText, message: 'server not accessible' } });
-                }).catch(error => reject({ error }))
-            })
+        this.config = await this.GetConfig().then(res => res);
+        let news = `https://geantworld.c1.is/api/rss`
+        let rss = await fetch(news).then(res => res.text());
+        let rssparse = JSON.parse(convert.xml2json(rss, { compact: true }));
+        let data = [];
+    
+        // Vérifier si des articles sont disponibles
+        if (rssparse.rss.channel.item) {
+            // Si c'est un tableau, parcourir chaque élément
+            if (Array.isArray(rssparse.rss.channel.item)) {
+                for (let i of rssparse.rss.channel.item) {
+                    let item = {}
+                    item.title = i.title._text;
+                    item.content = i['content:encoded']._text;
+                    item.author = i['dc:creator']._text;
+                    item.publish_date = i.pubDate._text;
+                    data.push(item);
+                }
+            } else {
+                // Sinon, il n'y a qu'un seul article, traitez-le comme un tableau
+                let item = {}
+                item.title = rssparse.rss.channel.item.title._text;
+                item.content = rssparse.rss.channel.item['content:encoded']._text;
+                item.author = rssparse.rss.channel.item['dc:creator']._text;
+                item.publish_date = rssparse.rss.channel.item.pubDate._text;
+                data.push(item);
+            }
         } else {
-            return new Promise((resolve, reject) => {
-                nodeFetch(news).then(async config => {
-                    if (config.status === 200) return resolve(config.json());
-                    else return reject({ error: { code: config.statusText, message: 'server not accessible' } });
-                }).catch(error => {
-                    return reject({ error });
-                })
-            })
+            // Aucun article disponible, ajoutez un message ou faites autre chose
+            data.push({
+                title: "Aucun article disponible",
+                content: "Aucun article n'a été trouvé.",
+                author: "News",
+                publish_date: "2023"
+            });
         }
+    
+        return data;
     }
 }
 
